ad8f924fe620e174b7a6513311369575c9e651ad
Guy Goudeau <guy.goudeau@students.aie.edu.au>
2016-11-15 16:50:27 -0600

    Set up triangles, wind now works but weird
    the wind makes the cloth go kinda deformed, but it works. Still need to
    break cloth and set boundaries

--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -13,11 +13,6 @@ public class Particle : MonoBehaviour
 
     public Vector3 updateParticle()
     {
-        foreach(Particle p in neighbors)
-        {
-            Debug.DrawLine(transform.position, p.transform.position, Color.green);
-        }
-
         acceleration = (1f / mass) * force;
         velocity += acceleration * Time.deltaTime;
         position += velocity * Time.deltaTime;
--- Assets/Scripts/clothSim.cs
+++ Assets/Scripts/clothSim.cs
@@ -6,10 +6,14 @@ public class clothSim : MonoBehaviour
 {
     List<Particle> particleList;
     List<springDamper> springDamperList;
+    List<springDamper> tempSpringDamperList;
     List<Triangle> triangleList;
+    List<Triangle> tempTriangleList;
     List<GameObject> lineList;
+    List<GameObject> tempLineList;
     public GameObject particlePrefab;
     public GameObject linePrefab;
+    public Material lineMaterial;
     public int width;
     public int height;
     [Range(0, 100)]
@@ -29,11 +33,15 @@ public class clothSim : MonoBehaviour
     {
         particleList = new List<Particle>();
         springDamperList = new List<springDamper>();
+        tempSpringDamperList = new List<springDamper>();
         triangleList = new List<Triangle>();
+        tempTriangleList = new List<Triangle>();
         lineList = new List<GameObject>();
+        tempLineList = new List<GameObject>();
 
         spawnParticles();
         setNeighborsAndDampers();
+        setTriangles();
 
         foreach (Particle p in particleList) // set starting position values for every particle
         {
@@ -43,6 +51,24 @@ public class clothSim : MonoBehaviour
 
     void Update()
     {
+        tempSpringDamperList = new List<springDamper>(); // reset the temporary line list
+        foreach (springDamper sd in springDamperList) // add every spring damper in the spring damper list to temporary spring damper list
+        {
+            tempSpringDamperList.Add(sd);
+        }
+
+        tempTriangleList = new List<Triangle>(); // reset the temporary line list
+        foreach (Triangle t in triangleList) // add every triangle in the triangle list to temporary triangle list
+        {
+            tempTriangleList.Add(t);
+        }
+
+        tempLineList = new List<GameObject>(); // reset the temporary line list
+        foreach (GameObject l in lineList) // add every line in the line list to temporary line list
+        {
+            tempLineList.Add(l);
+        }
+
         foreach (Particle p in particleList) // apply gravity to every particle
         {
             p.force = (gravity * Vector3.down) * p.mass;
@@ -56,12 +82,15 @@ public class clothSim : MonoBehaviour
             sd.computeForce();
         }
 
-        foreach (Triangle t in triangleList)
+        foreach (Triangle t in triangleList) // compute and apply aerodynamic force to every triangle
         {
-            t.AeroForce(airDensity, dragCoefficient, airVelocity * Vector3.forward);
+            if (airVelocity != 0)
+            {
+                t.AeroForce(airDensity, dragCoefficient, airVelocity * Vector3.forward);
+            }
         }
 
-        foreach (Particle p in particleList) // apply forward Euler integration to every particle that isn't an anchor
+        foreach (Particle p in particleList) // update the position of every particle that isn't an anchor
         {
             if (p.anchor == false)
             {
@@ -72,27 +101,36 @@ public class clothSim : MonoBehaviour
                 p.position = p.transform.position;
             }
         }
+
+        for (int i = 0; i < tempLineList.Count; i++) // set line positions, material and width
+        {
+            LineRenderer line = tempLineList[i].GetComponent<LineRenderer>();
+            line.SetPosition(0, springDamperList[i].P1.position);
+            line.SetPosition(1, springDamperList[i].P2.position);
+            line.material = lineMaterial;
+            line.SetWidth(.2f, .2f);
+        }
     }
 
-    public void spawnParticles()
+    public void spawnParticles() // creates grid of particles
     {
-        float xPos = 0f;
-        float yPos = 0f;
+        float xPos = 0f; // for determining position of particles on x axis 
+        float yPos = 0f; // for determining position of particles on y axis 
 
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                GameObject particle = Instantiate(particlePrefab, new Vector3(xPos, yPos, 0), new Quaternion()) as GameObject;
-                particleList.Add(particle.GetComponent<Particle>());
+                GameObject particle = Instantiate(particlePrefab, new Vector3(xPos, yPos, 0), new Quaternion()) as GameObject; // spawn particle prefab
+                particleList.Add(particle.GetComponent<Particle>()); // add that particle to the particle list
                 xPos += 2; // x margin of 2 between particles
             }
             yPos += 2; // y margin of 2 between particles
-            xPos = 0;
+            xPos = 0; // reset x for next loop
         }
 
-        particleList[particleList.Count - 1].anchor = true; // set top left anchor
-        particleList[particleList.Count - width].anchor = true; // set top right anchor
+        particleList[particleList.Count - 1].anchor = true; // set top right anchor
+        particleList[particleList.Count - width].anchor = true; // set top left anchor
     }
 
     public void setNeighborsAndDampers()
@@ -110,34 +148,66 @@ public class clothSim : MonoBehaviour
                 }
             } 
 
-            // remember how many heights you have is how many times you iterate through widths. create spring dampers for every neighbor
-            if (particleNum + width < particleList.Count) // this is the ups
+            // Note: how many heights you have is how many times you iterate through widths. create spring dampers for every neighbor
+            if (particleNum + width < particleList.Count) // above neighbor
             {
-                p.neighbors.Add(particleList[particleNum + width]);
-                springDamper sd = new springDamper(p, particleList[particleNum + width], springConstant, dampingFactor, restLength);
-                springDamperList.Add(sd);
+                p.neighbors.Add(particleList[particleNum + width]); // add that particle to the neighbor list
+                springDamper sd = new springDamper(p, particleList[particleNum + width], springConstant, dampingFactor, restLength); // create a spring damper
+                springDamperList.Add(sd); // add that spring damper to the spring damper list
+                lineList.Add(createLineRenderers(sd)); // create a line renderer here and add it to the line list
             }
 
-            if ((particleNum + 1) % width > particleNum % width) // this is the rights
+            if ((particleNum + 1) % width > particleNum % width) // immediate right neighbor
             {
-                p.neighbors.Add(particleList[particleNum + 1]);
-                springDamper sd = new springDamper(p, particleList[particleNum + 1], springConstant, dampingFactor, restLength);
-                springDamperList.Add(sd);
+                p.neighbors.Add(particleList[particleNum + 1]); // add that particle to the neighbor list
+                springDamper sd = new springDamper(p, particleList[particleNum + 1], springConstant, dampingFactor, restLength); // create a spring damper
+                springDamperList.Add(sd); // add that spring damper to the spring damper list
+                lineList.Add(createLineRenderers(sd)); // create a line renderer here and add it to the line list
             }
 
-            if (particleNum + width + 1 < particleList.Count && (particleNum + 1) % width > particleNum % width) // this is the top rights
+            if (particleNum + width + 1 < particleList.Count && (particleNum + 1) % width > particleNum % width) // top right neighbor
             {
-                p.neighbors.Add(particleList[particleNum + width + 1]);
-                springDamper sd = new springDamper(p, particleList[particleNum + width + 1], springConstant, dampingFactor, restLength);
-                springDamperList.Add(sd);
+                p.neighbors.Add(particleList[particleNum + width + 1]); // add that particle to the neighbor list
+                springDamper sd = new springDamper(p, particleList[particleNum + width + 1], springConstant, dampingFactor, restLength); // create a spring damper
+                springDamperList.Add(sd); // add that spring damper to the spring damper list
+                lineList.Add(createLineRenderers(sd)); // create a line renderer here and add it to the line list
             }
 
-            if (particleNum + width - 1 < particleList.Count && (particleNum + width - 1) % width < particleNum % width) // this is the top lefts
+            if (particleNum + width - 1 < particleList.Count && (particleNum + width - 1) % width < particleNum % width) // top left neighbor
             {
-                p.neighbors.Add(particleList[particleNum + width - 1]);
-                springDamper sd = new springDamper(p, particleList[particleNum + width - 1], springConstant, dampingFactor, restLength);
-                springDamperList.Add(sd);
+                p.neighbors.Add(particleList[particleNum + width - 1]); // add that particle to the neighbor list
+                springDamper sd = new springDamper(p, particleList[particleNum + width - 1], springConstant, dampingFactor, restLength); // create a spring damper
+                springDamperList.Add(sd); // add that spring damper to the spring damper list
+                lineList.Add(createLineRenderers(sd)); // create a line renderer here and add it to the line list
             }
         }
+    }
+
+    public void setTriangles()
+    {
+        // i = this particle
+        // i + 1 = right
+        // i + width = top
+        // i + width - 1 = top left
+
+        for (int i = 0; i < particleList.Count; i++)
+        {
+            if (i % width != width - 1 && i < (width * height) - width) // bottom left triangle
+            {
+                Triangle tri = new Triangle(particleList[i], particleList[i + 1], particleList[i + width]); // (bottom, right, top)
+                triangleList.Add(tri);
+            }
+            if (i % width != 0 && i < (width * height) - width) // top right triangle
+            {
+                Triangle tri = new Triangle(particleList[i], particleList[i + width - 1], particleList[i + width]); // (bottom, top left, top)
+                triangleList.Add(tri);
+            }
+        }
+    }
+
+    public GameObject createLineRenderers(springDamper sd)
+    {
+        GameObject lineGO = Instantiate(linePrefab, (sd.P1.position + sd.P2.position) / 2, new Quaternion()) as GameObject; // use line prefab to draw from first particle to second particle
+        return lineGO;
     }
 }
